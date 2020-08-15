53c5f8868e713517eacf006c301d18cfb4a64d2f
Jeannette <jeannetteshui.ng@gmail.com>
2020-08-15 12:28:45 -0400

    Updates

---
+++ precache-manifest.4d67c7bfbacd590fe298325f5727fd06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b633a2de5b3be9e4bce5c0b921f84d88",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "72e77d80515eb444c8ba",
+    "url": "/calculator/static/css/main.ad3c6d85.chunk.css"
+  },
+  {
+    "revision": "1131e15a36fa780e1eaf",
+    "url": "/calculator/static/js/2.b7d6e305.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.b7d6e305.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72e77d80515eb444c8ba",
+    "url": "/calculator/static/js/main.b210f54d.chunk.js"
+  },
+  {
+    "revision": "d3fc2a2896294ca2ab3f",
+    "url": "/calculator/static/js/runtime-main.99dd7910.js"
+  }
+]);
